cb0756f2f1d7786ba528d40d4fbb95f0ac17113a
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-18 01:41:43 -0400

    Exercise 1 printing on shell

---
+++ Day 1/ExerciseLevel1.py
@@ -0,0 +1,48 @@
+# >>> 3 + 4
+# 7
+# >>> 3 - 4
+# -1
+# >>> 3 * 4
+# 12
+# >>> 3 % 4
+# 3
+# >>> 3 / 4
+# 0.75
+# >>> 3 ** 4
+# 81
+# >>> 3 // 4
+# 0
+# >>> name = 'Joseph'  
+# >>> surname = 'Rodriguez Kelly' 
+# >>> print(name)
+# Joseph
+# >>> name
+# 'Joseph'
+# >>> countr = 'Dominican Republic' 
+# >>> country = 'Dominican Republic' 
+# >>> del countr
+# >>> countr
+# Traceback (most recent call last):      
+#   File "<stdin>", line 1, in <module>   
+# NameError: name 'countr' is not defined. Did you mean: 'country'?
+# >>> country    
+# 'Dominican Republic'
+# >>> string = 'I am enjoying 30 days of python' 
+# >>> string
+# 'I am enjoying 30 days of python'       
+# >>> type(10)
+# <class 'int'>
+# >>> type(9.8)
+# <class 'float'>
+# >>> type(3.14)
+# <class 'float'>
+# >>> type(4-4j)
+# <class 'complex'>
+# >>> type(['Asabeneh', 'Python', 'Finland']) 
+# <class 'list'>
+# >>> type(name)
+# <class 'str'>
+# >>> type(surname)
+# <class 'str'>
+# >>> type(country)
+# <class 'str'>           
