ed6d42f13a87501160e30d0f16e13d7d6e29d8b6
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-20 19:29:22 -0400

    to find values and similar to equal as java

--- Day 3/ExampleComparisonOperators.py
+++ Day 3/ExampleComparisonOperators.py
@@ -19,3 +19,14 @@ print(len('python') > len('dragon'))   # False
 print('True == True: ', True == True)
 print('True == False: ', True == False)
 print('False == False:', False == False)
+
+print('1 is 1', 1 is 1)                   # True - because the data values are the same
+print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
+print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
+print('B in Asabeneh', 'B' in 'Asabeneh') # False - there is no uppercase B
+print('coding' in 'coding for all') # True - because coding for all has the word coding
+print('a in an:', 'a' in 'an')      # True
+print('4 is 2 ** 2:', 4 is 2 ** 2)   # True
+print('a is a', 'a' is 'a')  
+print('true is true', 'true' is 'true') # Check is both are equal
+
