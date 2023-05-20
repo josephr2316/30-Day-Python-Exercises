407d10f0929a7c043b26591abe17667b79e7cdc2
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-19 23:20:15 -0400

    finishing the exercises

---
+++ Day 2/variables.py
@@ -0,0 +1,55 @@
+# Day 2: 30 Days of python programming
+
+import math
+
+first_name = 'Joseph'
+last_name = 'Rodriguez Kelly'
+full_name = 'Joseph Rodriguez Kelly'
+country = 'Dominican Republic'
+city = 'Moca'
+age = 28
+year = 2023
+is_married = False
+is_true = True
+is_light_on = False
+
+a, b, c, d, e = 1, 2, 3, 4, 5
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+print(len(first_name))
+print(len(last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+area_of_circle = 0
+circum_of_circle = 0
+r = int(input('Insert the radius of the circle: ')) 
+area_of_circle = math.pi * pow (r,2)
+circum_of_circle = 2 * math.pi * r
+first_name = input('Insert your firstname: ')
+last_name = input('Insert your firstname: ')
+country = input("Insert the name of your country: ") 
+age = int(input('Insert your age: '))
+help('keywords')
