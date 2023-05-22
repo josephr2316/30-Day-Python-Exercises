5272c6eb23b140851c6b8cbb4e4a741ee10972b6
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-22 03:13:57 -0400

    escape

---
+++ Day 4/escape.py
@@ -0,0 +1,16 @@
+print('I hope everyone is enjoying the Python Challenge.\nAre you ?') # line break
+print('Days\tTopics\tExercises') # adding tab space or 4 spaces 
+print('Day 1\t3\t5')
+print('Day 2\t3\t5')
+print('Day 3\t3\t5')
+print('Day 4\t3\t5')
+print('This is a backslash  symbol (\\)') # To write a backslash
+print('In every programming language it starts with \"Hello, World!\"') # to write a double quote inside a single quote
+
+# \n: new line
+# \t: Tab means(8 spaces)
+# \\: Back slash
+# \': Single quote (')
+# \": Double quote (")
+
+print('\t hola') #  Tab at the begining is 8 space, after a character is 4 space
