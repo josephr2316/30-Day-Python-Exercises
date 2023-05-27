63b70f2a02d3417825e26019518be094b8ce36dd
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 14:24:27 -0400

    removing elements from the list

---
+++ Day 5/remove_items_list.py
@@ -0,0 +1,40 @@
+# syntax
+lst = ['item1', 'item2']
+item = "no item"
+lst.remove(item)
+
+fruits = ['banana', 'orange', 'mango', 'lemon', 'banana']
+fruits.remove('banana')
+print(fruits)  # ['orange', 'mango', 'lemon', 'banana'] - this method removes the first occurrence of the item in the list
+fruits.remove('lemon')
+print(fruits)  # ['orange', 'mango', 'banana']
+
+# Removing Items Using Pop
+# syntax
+lst = ['item1', 'item2']
+lst.pop()       # last item
+index = 2
+lst.pop(index)
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.pop()
+print(fruits)       # ['banana', 'orange', 'mango']
+
+fruits.pop(0)
+print(fruits)       # ['orange', 'mango']
+
+# Removing Items Using Del
+# syntax
+lst = ['item1', 'item2']
+del lst[index] # only a single item
+del lst        # to delete the list completely
+
+
+fruits = ['banana', 'orange', 'mango', 'lemon', 'kiwi', 'lime']
+del fruits[0]
+print(fruits)       # ['orange', 'mango', 'lemon', 'kiwi', 'lime']
+del fruits[1]
+print(fruits)       # ['orange', 'lemon', 'kiwi', 'lime']
+del fruits[1:3]     # this deletes items between given indexes, so it does not delete the item with index 3!
+print(fruits)       # ['orange', 'lime']
+del fruits
+print(fruits)       # This should give: NameError: name 'fruits' is not defined
