7df64f8b27cfcbd570677ec8952ecd1243c13227
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 15:05:36 -0400

    finding index of an item in a list

---
+++ Day 5/find_index_item_list.py
@@ -0,0 +1,9 @@
+# syntax
+lst = ['item1', 'item2']
+item = 'item1'
+
+lst.index(item)
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits.index('orange'))   # 1
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print(ages.index(24))           # 2, the first occurrence
