719b9f1981cb442e385d30cd1c61d1dddb38ed92
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 14:17:30 -0400

    adding more items

--- Day 5/add_items_list.py
+++ Day 5/add_items_list.py
@@ -2,3 +2,9 @@
 item = "9"
 lst = list()
 lst.append(item)
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.append('apple')
+print(fruits)           # ['banana', 'orange', 'mango', 'lemon', 'apple']
+fruits.append('lime')   # ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime']
+print(fruits)
