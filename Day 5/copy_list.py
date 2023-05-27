11f292334dbb338b667ffe2ceccce06152666894
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 14:26:09 -0400

    copying a list

---
+++ Day 5/copy_list.py
@@ -0,0 +1,7 @@
+# syntax
+lst = ['item1', 'item2']
+lst_copy = lst.copy()
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits_copy = fruits.copy()
+print(fruits_copy)       # ['banana', 'orange', 'mango', 'lemon']
