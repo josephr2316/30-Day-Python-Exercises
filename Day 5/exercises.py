9d09e94a690a004809b2dc81914cf2cf630f1fb7
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 15:24:40 -0400

    printing some sort of elements of the list

--- Day 5/exercises.py
+++ Day 5/exercises.py
@@ -7,3 +7,14 @@ list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # 3.Find the length of your list
 print("Length of the list: ", len(list2))
+
+# 4.Get the first item, the middle item and the last item of the list
+first_item = list2[0]
+middle_index = len(list2) // 2
+last_index = len(list2) - 1
+middle_item = list2[middle_index]
+last_item = list2[last_index]
+
+print("The first item of the list is: ", first_item)
+print("The middle item of the list is: ", middle_item)
+print("The last item of the list is : ", last_item)
