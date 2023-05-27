86a813a19bc98af4316cf52501b70246bf720e69
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 14:20:46 -0400

    Inserting elements in a list

---
+++ Day 5/intert_items_list.py
@@ -0,0 +1,12 @@
+# syntax
+lst = ['item1', 'item2']
+index = 2
+item = 'new item'
+lst.insert(index, item)
+
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.insert(2, 'apple') # insert apple between orange and mango
+print(fruits)           # ['banana', 'orange', 'apple', 'mango', 'lemon']
+fruits.insert(3, 'lime')   # ['banana', 'orange', 'apple', 'lime', 'mango', 'lemon']
+print(fruits)
