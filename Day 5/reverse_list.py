bd5b865093216558e9958a9c8803dceec94f09f3
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 15:06:15 -0400

    reverse list

---
+++ Day 5/reverse_list.py
@@ -0,0 +1,10 @@
+# syntax
+lst = ['item1', 'item2']
+lst.reverse()
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.reverse()
+print(fruits) # ['lemon', 'mango', 'orange', 'banana']
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.reverse()
+print(ages) # [24, 25, 24, 26, 25, 24, 19, 22]
