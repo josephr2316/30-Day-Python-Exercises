5741bfdf6986f755c1cd6a1807a5e9912b01970c
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 15:04:12 -0400

    count items in a list

---
+++ Day 5/count_items_list.py
@@ -0,0 +1,8 @@
+# syntax
+lst = ['item1', 'item2']
+item = 'item1'
+lst.count(item)
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits.count('orange'))   # 1
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print(ages.count(24))           # 3
