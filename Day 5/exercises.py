2dd500b7c78bd1c5823e935762ad634f09dccff0
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2023-05-27 15:20:06 -0400

    print the length of the list

--- Day 5/exercises.py
+++ Day 5/exercises.py
@@ -1,3 +1,9 @@
 # 1.Declare an empty list
 lst = list()
 list1 = []
+
+# 2.Declare a list with more than 5 items
+list2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# 3.Find the length of your list
+print("Length of the list: ", len(list2))
